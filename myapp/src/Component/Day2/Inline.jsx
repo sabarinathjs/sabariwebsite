3613bf552671bca6ff457a463c19bc17e09ca5eb
Sabari Nath.J.S. <rockzzttt100@gmail.com>
2023-11-17 10:28:01 +0530

    save

--- myapp/src/Component/Day2/Inline.jsx
+++ myapp/src/Component/Day2/Inline.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function Inline() {
-  return (
-    <div>
-      <h1>Inline</h1></div>
-  )
+const Inline = () => {
+    return(
+        <div style = {{backgroundColor : "lightblue", padding : "10 px" , border : "4px solid blue" , borderRadius : "5px"}}>
+        <h1 style = {{color : "green" , marginLeft : "570px"}}>InLine Style in CSS Example</h1>
+            <p style = {{color : "darkblue" , fontSize : "16px"}}><center>This the Paragraph text with InLine styles applied.</center></p>
+        </div>
+    );
 }
 
 export default Inline;
